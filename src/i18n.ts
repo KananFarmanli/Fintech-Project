0bdd335e432fcb4d5e30d02f91f05eb54029da3e
Kanan Farmanli <farmanlikanan@gmail.com>
2024-06-13 10:26:15 +0400

    language issues fixed

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import english from "./locales/en/translation.json";
 import russian from "./locales/ru/translation.json";
+import turkish from "./locales/tr/translation.json";
 
 i18n.use(initReactI18next).init({
   debug: true,
@@ -15,6 +16,9 @@ i18n.use(initReactI18next).init({
     },
     ru: {
       translation: russian,
+    },
+    tr: {
+      translation: turkish,
     },
   },
 });
